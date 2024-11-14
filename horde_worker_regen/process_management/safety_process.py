2408074d01fb242312b757eb2c1a860fc99d16be
gabriel20xx <gabrielfranz31@gmail.com>
2024-11-14 23:08:38 +0100

    fix

--- horde_worker_regen/process_management/safety_process.py
+++ horde_worker_regen/process_management/safety_process.py
@@ -207,7 +207,7 @@ class HordeSafetyProcess(HordeProcess):
                 # Create a PngInfo object to hold metadata
                 metadata = PngImagePlugin.PngInfo()
 
-                if "###" in message.prompt:
+                if " ### " in message.prompt:
                     # Split the text at "###"
                     parts = message.prompt.split(" ### ")
 
